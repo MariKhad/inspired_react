9fd8b6c33480169b896cc460772533104b0735ba
Marina Khadieva <utabond89@gmail.com>
2023-07-05 18:09:32 +0300

    1day

---
+++ src/Components/Footer/Footer.jsx
@@ -0,0 +1,9 @@
+import { Top } from "./Top/Top";
+import { Navigation } from "./Navigation/Navigation";
+
+export const Footer = () => (
+	<footer>
+		<Top />
+		<Navigation />
+	</footer>
+)
---
+++ src/Components/Header/Navigation/Navigation.jsx
@@ -0,0 +1,13 @@
+import { Category } from "./Category/Category";
+import { Gender } from "./Gender/Gender";
+
+
+
+export const Navigation = () => (
+	<nav>
+		<div className="container">
+			<Gender />
+			<Category />
+		</div>
+	</nav>
+);
